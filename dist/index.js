e799e7390fd15fde512d6c04333f175b3f628db3
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-09-05 15:33:28 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -105,7 +105,8 @@ function run() {
         try {
             const ktPath = yield tc.downloadTool('https://github.com/JetBrains/kotlin/releases/download/v1.4.0/kotlin-compiler-1.4.0.zip');
             const ktPathExtractedFolder = yield tc.extractZip(ktPath, 'kotlin-compiler');
-            exec.exec(ktPathExtractedFolder + "/bin/kotlinc", ["-version"]);
+            core.addPath(`${ktPathExtractedFolder}/bin`);
+            exec.exec("kotlinc", ["-version"]);
         }
         catch (error) {
             core.setFailed(error.message);
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function run(): Promise<void> {
   try {
     const ktPath = await tc.downloadTool('https://github.com/JetBrains/kotlin/releases/download/v1.4.0/kotlin-compiler-1.4.0.zip');
     const ktPathExtractedFolder = await tc.extractZip(ktPath, 'kotlin-compiler');
-    exec.exec(ktPathExtractedFolder + "/bin/kotlinc", ["-version"])
+    core.addPath(`${ktPathExtractedFolder}/bin`);
+    exec.exec("kotlinc", ["-version"])
   } catch (error) {
     core.setFailed(error.message)
   }
