970441f317b24af82349dc990dcbe6caa4065b30
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2020-09-05 21:35:51 +0200

    Bootstrap Action for installing Kotlin (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const ktPath = await tc.downloadTool(
+      'https://github.com/JetBrains/kotlin/releases/download/v1.4.0/kotlin-compiler-1.4.0.zip'
+    )
+    const ktPathExtractedFolder = await tc.extractZip(ktPath, 'kotlin-compiler')
+    core.addPath(`${ktPathExtractedFolder}/kotlinc/bin`)
+    exec.exec('kotlinc', ['-version'])
   } catch (error) {
     core.setFailed(error.message)
   }
