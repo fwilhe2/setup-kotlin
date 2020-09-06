7d25985ad0df25611acaf0f3da84e636201564dd
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-09-06 22:36:48 +0200

    update

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,7 +5,6 @@ import * as path from 'path'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_SCRIPT'] = 'println(234234)'
-  process.env['INPUT_VERSION'] = '1.4.0'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
--- dist/index.js
+++ dist/index.js
@@ -104,10 +104,7 @@ const fs = __importStar(__webpack_require__(747));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            let version = core.getInput('version');
-            if (!version) {
-                version = '1.4.0';
-            }
+            const version = core.getInput('version');
             let cachedPath = tc.find('kotlin', version);
             if (!cachedPath) {
                 core.debug(`Could not find Kotlin ${version} in cache, downloading it.`);
