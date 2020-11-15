1edb225786dfb8eeeec3953db58ea16b411b6676
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-11-15 14:42:56 +0100

    update

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -9,18 +9,6 @@ process.env['RUNNER_TOOL_CACHE'] = toolDir
 process.env['RUNNER_TEMP'] = tempDir
 
 // shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   process.env['INPUT_SCRIPT'] = 'println(234234)'
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecSyncOptions = {
-//     env: process.env
-//   }
-//   const output = cp.execSync(`node ${ip}`, options).toString().split('\n')
-//   console.log(output)
-//   console.log(output[output.length - 2])
-//   expect(output[output.length - 2]).toEqual('234234')
-// })
-
 test('test runs with explicit version', () => {
   process.env['INPUT_SCRIPT'] = 'println(234234)'
   process.env['INPUT_VERSION'] = '1.3.72'
