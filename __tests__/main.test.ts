d3099dd23d1ef90a9457ef43a8efef98f3795641
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2026-02-06 20:02:20 +0100

    test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,6 @@
 import {getInputInstallNative} from '../src/main'
 import * as process from 'process'
-import * as cp from 'child_process'
 import * as path from 'path'
-import {writeFileSync} from 'fs'
 import {expect, test} from '@jest/globals'
 
 const toolDir = path.join('runner', 'tools')
@@ -21,19 +19,4 @@ test('getInputInstallNative string false', async () => {
 
 test('getInputInstallNative string empty', async () => {
   expect(getInputInstallNative('')).toStrictEqual(true)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('runs with explicit version', () => {
-  process.env['INPUT_SCRIPT'] = 'println(234234)'
-  process.env['INPUT_VERSION'] = '2.3.0'
-  process.env['INPUT_install-native'] = 'false'
-
-  writeFileSync('.env', 'RUNNER_TOOL_CACHE=__tests__/runner/tools')
-
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  const output = cp.execSync(`npm run local-action`, options).toString()
-  expect(output).toMatch(/kotlinc-jvm 2.3.0/)
 })
